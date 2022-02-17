d2a580960d03fca9585ed8ffd3a12d8db4e76111
Ahsan Irfan <ahsan.irfan@gmail.com>
2022-02-17 15:19:40 +0000

    refactored json the cat

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,10 @@
 const request = require('request');
 
-request('https://api.thecatapi.com/v1/breeds/search?q=' + process.argv[2], (error, response, body) => {
-  if (!error && response.statusCode === 200) {
+const fetchBreedDescription = function(breedName, callback) {
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, (error, response, body) => {
     const data = JSON.parse(body);
-    if (data[0] !== undefined) {
-      console.log(data[0].description);
-    } else {
-      console.log('This breed does not exist. Please enter another and try again.');
-    }
-  } else {
-    console.log('Ooops! Something went wrong on our end! Please try again later.');
-  }
-  
-});
+    callback(error, data[0].description);
+  });
+};
+
+module.exports = { fetchBreedDescription };
