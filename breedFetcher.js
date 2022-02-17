739d3ca1313dfa15a92c76aa04ebcc14cd103442
Ahsan Irfan <ahsan.irfan@gmail.com>
2022-02-17 15:56:19 +0000

    added tests for json the cat

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,8 +2,18 @@ const request = require('request');
 
 const fetchBreedDescription = function(breedName, callback) {
   request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, (error, response, body) => {
+    let err = null;
     const data = JSON.parse(body);
-    callback(error, data[0].description);
+    let desc = null;
+
+    if (body === undefined) {
+      err = 'Oops! Something went wrong on our end! Please try again later';
+    } else if (data[0] === undefined) {
+      err = 'Breed does not exist. Please enter another breed.'
+    } else {
+      desc = data[0].description;
+    }
+    callback(err, desc);
   });
 };
 
